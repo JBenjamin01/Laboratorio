3668ab7bfdd258d632cf933f46eac54877cbbf48
= <=>
2023-10-08 00:09:41 -0500

    Cambio del tipo int a char para la columna, optimización para la clase principal

--- Actividades de laboratorio/Lab07/Soldado.java
+++ Actividades de laboratorio/Lab07/Soldado.java
@@ -2,45 +2,39 @@ public class Soldado {
     private String nombre;
     private int puntosVida;
     private int fila;
-    private int columna;
+    private char columna;
 
-    public Soldado(String nombre, int puntosVida, int fila, int columna) {
+    public Soldado(String nombre, int puntosVida, int fila, char columna) {
         this.nombre = nombre;
         this.puntosVida = puntosVida;
         this.fila = fila;
+        this.columna = columna;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setPuntosVida(int puntosVida) {
+        this.puntosVida = puntosVida;
+    }
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+    public void setColumna(char columna) {
         this.columna = columna;
     }
 
     public String getNombre() {
         return nombre;
     }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
     public int getPuntosVida() {
         return puntosVida;
     }
-
-    public void setPuntosVida(int puntosVida) {
-        this.puntosVida = puntosVida;
-    }
-
     public int getFila() {
         return fila;
     }
-
-    public void setFila(int fila) {
-        this.fila = fila;
-    }
-
-    public int getColumna() {
+    public char getColumna() {
         return columna;
-    }
-
-    public void setColumna(int columna) {
-        this.columna = columna;
     }
 
     public String toString() {
